e2589fb368389c8ee205531f7fe0cc925f9a7a14
CL75 <funnotfame@gmail.com>
2013-06-23 22:25:20 -0700

    quick test to see if using attr_accessible prevents heroku deployment

--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,7 +1,7 @@
 class Sticker < ActiveRecord::Base
   # You will need to use attr_accessible if you are
     # using Rails config setting `whitelist_attributes = true`
-   # attr_accessible :image
+    attr_accessible :image
 
     # This method associates the attribute ":image" with a file attachment
     has_attached_file :image, styles: {
